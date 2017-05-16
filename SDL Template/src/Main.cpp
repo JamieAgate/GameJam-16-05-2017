b9baf5f7eff61d248027aa4ad81e36dddd552cc2
Joseph Bluck <bluckjoseph@gmail.com>
2017-05-16 20:14:30 +0100

    Added in selection menu for type

--- SDL Template/src/Main.cpp
+++ SDL Template/src/Main.cpp
@@ -5,6 +5,9 @@
 #define WINDOWHEIGHT 1280 //Configure values for screen size here
 #define WINDOWWIDTH 720
 
+void GameLoop();
+void ServerLoop();
+
 int main(int arc, char* args[])
 {
 	SDL_Window* window = NULL;
@@ -18,11 +21,38 @@ int main(int arc, char* args[])
 		return -2;
 	}
 
-	if (!SetupRenderer(window, renderer, WINDOWHEIGHT, WINDOWWIDTH)) {
-		CloseSDL(window, renderer);
-		return -3;
+	char choice = ' ';
+
+	while (choice != '1' && choice != '2') {
+		std::cout << "Welcome to Holiday Hell! Please choose an option,\n1. Start Game\n2. Start Server\n";
+		std::cin >> choice;
+	}
+
+	if (choice == '1') {
+		if (!SetupRenderer(window, renderer, WINDOWHEIGHT, WINDOWWIDTH)) {
+			CloseSDL(window, renderer);
+			return -3;
+		}
+
+		GameLoop();
+	}
+	else {
+		ServerLoop();
+		CloseSDL(NULL, NULL);
 	}
 
 	system("pause");
 	return 0;
+}
+
+//Game Loop
+void GameLoop()
+{
+
+}
+
+//Server Loop
+void ServerLoop()
+{
+
 }
